b71a9be44024238377699e3c1cfcd54e16c97a41
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-05 03:34:10 -0400

    Wrote out code and comments.

--- main.py
+++ main.py
@@ -18,4 +18,49 @@ from turtle import *
 # Draw circle 30 px
 # End
 
+speed(0)
+
+bgcolor("black")
+
+
+# Create the ORANGE planet
+color("orange")
+begin_fill()
+circle(80)
+end_fill()
+
+# Move Forwards
+penup()
+forward(100)
+pendown()
+
+# Create the GREY planet
+color("grey")
+begin_fill()
+circle(20)
+end_fill()
+
+# Move Forwards
+penup()
+forward(80)
+pendown()
+
+# Create the RED planet
+color("red")
+begin_fill()
+circle(40)
+end_fill()
+
+# Move Forwards
+penup()
+forward(90)
+pendown()
+
+# Create the GREEN planet
+color("green")
+begin_fill()
+circle(30)
+end_fill()
+
+done()
 
