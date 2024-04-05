dcb8d0ec48608218708d3825581cbfa6e6632024
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-05 03:17:32 -0400

    Made diagrams for project and wrote pseudocode.

---
+++ main.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+# Start
+# Begin fill
+# Set fill color and fill to orange
+# Draw circle 80px
+# Pen up and forward 100px
+# Pen down
+# Set fill color and fill to grey
+# Draw circle 20 px
+# Pen up and forward 80px
+# Pen down
+# Set fill color and fill to red
+# Draw circle 40 px
+# Pen up and forward 90px
+# Pen down
+# Set fill color and fill to green
+# Draw circle 30 px
+# End
+
+
